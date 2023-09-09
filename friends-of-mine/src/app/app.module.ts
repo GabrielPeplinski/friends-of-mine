bc5334a22659e6d6c9fe4722caba95c0d3beac71
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2023-09-08 23:54:46 -0300

    Ajustes e funções adicionadas

--- friends-of-mine/src/app/app.module.ts
+++ friends-of-mine/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/sidebar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
--- friends-of-mine/src/app/components/navbar/navbar.component.ts
+++ friends-of-mine/src/app/components/navbar/navbar.component.ts
@@ -8,13 +8,7 @@ import {Component} from '@angular/core';
 export class NavbarComponent {
   title: string = 'Friends Of Mine'
 
-  onTextClick(): void {
-    window.alert('Evento adicionado')
-  }
-
-  showMenu = true;
-
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
+  onTitleClick(): void {
+    console.log('Botão para tela inicial clicado')
   }
 }
