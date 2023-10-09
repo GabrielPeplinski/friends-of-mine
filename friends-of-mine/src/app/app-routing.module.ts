de8db88151a0e5a537017c86f2ab839d525f1de4
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2023-10-08 23:26:47 -0300

    Tela de cadastro de contatos adicionado

--- friends-of-mine/src/app/app-routing.module.ts
+++ friends-of-mine/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CreateContactPageComponent} from "./pages/create-contact-page/create-contact-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'novo-contato', component: CreateContactPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
