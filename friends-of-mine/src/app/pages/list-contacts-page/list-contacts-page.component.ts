20bec804d0d5e90c2e3817b23a27b06b8d11827a
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2023-10-31 15:11:27 -0300

    Listagem de itens adicionada

--- friends-of-mine/src/app/pages/list-contacts-page/list-contacts-page.component.ts
+++ friends-of-mine/src/app/pages/list-contacts-page/list-contacts-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ContactService} from "@services/contact/contact.service";
+import {Router} from "@angular/router";
+import {ContactInterface} from "@interfaces/ContactInterface";
 
 @Component({
   selector: 'app-list-contacts-page',
   templateUrl: './list-contacts-page.component.html',
   styleUrls: ['./list-contacts-page.component.css']
 })
-export class ListContactsPageComponent {
 
+export class ListContactsPageComponent implements OnInit {
+  public contacts: ContactInterface[] = [];
+
+  constructor(private contactService: ContactService, private router: Router) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    const params: any = '';
+
+    (await this.contactService.index(params))
+      .subscribe({
+        next: (contacts: ContactInterface[]) => this.contacts = contacts,
+        error: (e) => console.error(e),
+      })
+  }
 }
