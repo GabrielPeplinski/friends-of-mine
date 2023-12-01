214165cc490277c7a3b9c525166a81e51b0d6917
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2023-12-01 00:19:05 -0300

    Remoção config inválid

--- friends-of-mine/src/app/app-routing.module.ts
+++ friends-of-mine/src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
